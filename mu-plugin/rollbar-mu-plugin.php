3c20d1964306211f7025a9b91edbc122bacbc154
Daniel Morell <daniel@cairntek.com>
2026-09-12 09:39:29 -0500

    MU plugin maintenance: - Added MU plugin ABSPATH security check - Reduced verbose name - Fixed WP env using project directory name for plugin name

--- mu-plugin/rollbar-mu-plugin.php
+++ mu-plugin/rollbar-mu-plugin.php
@@ -1,14 +1,17 @@
 <?php
 
 /**
- * Plugin Name: Rollbar PHP WordPress Must Use Plugin
+ * Plugin Name: Rollbar
  * Plugin URI: https://github.com/rollbar/rollbar-php-wordpress
  * Description: "Must-use" proxy plugin for Rollbar PHP WordPress.
  * Author: Rollbar
  * Author URI: https://rollbar.com
  */
 
-$rollbar_plugin = __DIR__ . '/../plugins/rollbar/rollbar.php';
+// phpcs:disable PSR1.Files.SideEffects.FoundWithSymbols
+// Exit if accessed directly
+defined('ABSPATH') || exit;
+
 $rollbar_plugin = dirname(__DIR__) . '/plugins/rollbar/rollbar.php';
 
 if (!file_exists($rollbar_plugin)) {
